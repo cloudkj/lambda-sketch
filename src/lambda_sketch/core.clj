7b6e45dfb89b3880f7d07241a3a091e2a4d794cc
cloudkj <cloudkj@users.noreply.github.com>
2026-05-19 11:44:58 -0700

    Initial Bloom Filter implementation

--- src/lambda_sketch/core.clj
+++ src/lambda_sketch/core.clj
@@ -1,6 +1,40 @@
-(ns lambda-sketch.core)
+(ns lambda-sketch.core
+  (:import [com.google.common.hash Hashing]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn to-bytes
+  [obj]
+  (with-open [baos (java.io.ByteArrayOutputStream.)
+              oos (java.io.ObjectOutputStream. baos)]
+    (.writeObject oos obj)
+    (.toByteArray baos)))
+
+;; TODO: add support for other types
+(defn seed-hash
+  [seed]
+  (let [h (Hashing/murmur3_128 seed)]
+    (fn [x]
+      (cond
+        ;;(instance? java.lang.Double x) (-> h .newHasher (.putDouble x) .hash .asLong)
+        ;;(instance? java.lang.Float x) (-> h .newHasher (.putFloat x) .hash .asLong)
+        ;;(instance? java.lang.Integer x) (-> h .newHasher (.putInt x) .hash .asLong)
+        (instance? java.lang.Long x) (-> h .newHasher (.putLong x) .hash .asLong bigint)
+        :else (throw (UnsupportedOperationException. (str "TODO:" (class x))))))))
+
+(defn bloom-filter
+  [m k]
+  ;; For now, underlying data represented as map of internal data structures
+  (let [h1 (seed-hash 1)
+        h2 (seed-hash 2)]
+    {:bits (java.util.BitSet. m)
+     :hash-fns (for [i (range k)] (fn [x] (int (mod (+ (h1 x) (* i (h2 x))) m))))}))
+
+(defn bloom-filter-add
+  [bf x]
+  (assoc bf :bits
+         (reduce (fn [b h] (doto b (.set (h x))))
+                 (:bits bf)
+                 (:hash-fns bf))))
+
+(defn bloom-filter-test
+  [bf x]
+  (not (some false? (map #(.get (:bits bf) (% x)) (:hash-fns bf)))))
--- test/lambda_sketch/core_test.clj
+++ test/lambda_sketch/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [lambda-sketch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-empty
+  (testing "An empty Bloom filter should return false for all queries"
+    (let [bf (bloom-filter 100 5)]
+      (is (false? (bloom-filter-test bf 1)))
+      (is (false? (bloom-filter-test bf 2)))
+      (is (false? (bloom-filter-test bf 100))))))
+
+(deftest test-add
+  (testing "Elements added to the filter should always return true, i.e. no false negatives"
+    (let [bf (-> (bloom-filter 100 5)
+                 (bloom-filter-add 1)
+                 (bloom-filter-add 2)
+                 (bloom-filter-add 100))]
+      (is (true? (bloom-filter-test bf 1)))
+      (is (true? (bloom-filter-test bf 2)))
+      (is (true? (bloom-filter-test bf 100))))))
