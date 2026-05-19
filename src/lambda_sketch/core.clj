515eea4f2fec0792b9cbb9e70bf32c7a12094068
cloudkj <cloudkj@users.noreply.github.com>
2026-05-19 16:33:33 -0700

    Count min sketch impl

--- src/lambda_sketch/core.clj
+++ src/lambda_sketch/core.clj
@@ -1,6 +1,8 @@
 (ns lambda-sketch.core
   (:import [com.google.common.hash Hashing]))
 
+;; TODO: expand hashing support for Clojure types
+
 (defn seed-hash
   [seed]
   (let [h (Hashing/murmur3_128 seed)]
@@ -10,9 +12,10 @@
         (instance? java.lang.Long x) (bigint (.asLong (.hashLong h x)))
         :else (throw (UnsupportedOperationException. (str "TODO:" (class x))))))))
 
+;; TODO: consider implementing data structures as IPersistentCollection for cons, etc
+
 (defn bloom-filter
   [m k]
-  ;; For now, underlying data represented as map of internal data structures
   (let [h1 (seed-hash 1)
         h2 (seed-hash 2)]
     {:bits (java.util.BitSet. m)
@@ -28,3 +31,25 @@
 (defn bloom-filter-test
   [bf x]
   (not (some false? (map #(.get (:bits bf) (% x)) (:hash-fns bf)))))
+
+(defn count-min-sketch
+  [w d]
+  (let [h1 (seed-hash 1)
+        h2 (seed-hash 2)]
+    {:arrays (repeatedly d #(long-array w))
+     :hash-fns (for [i (range d)] (fn [x] (int (mod (+ (h1 x) (* i (h2 x))) w))))}))
+
+(defn count-min-sketch-add
+  [cms x]
+  (assoc cms :arrays
+         (map (fn [a h] (let [index (h x)
+                              curr (aget a index)]
+                          (doto a (aset index (inc curr)))))
+              (:arrays cms)
+              (:hash-fns cms))))
+
+(defn count-min-sketch-get
+  [cms x]
+  (apply min (map (fn [a h] (aget a (h x)))
+                  (:arrays cms)
+                  (:hash-fns cms))))
--- test/lambda_sketch/core_test.clj
+++ test/lambda_sketch/core_test.clj
@@ -2,14 +2,14 @@
   (:require [clojure.test :refer :all]
             [lambda-sketch.core :refer :all]))
 
-(deftest test-empty
+(deftest test-bloom-filter-empty
   (testing "An empty Bloom filter should return false for all queries"
     (let [bf (bloom-filter 100 5)]
       (is (false? (bloom-filter-test bf 1)))
       (is (false? (bloom-filter-test bf 2)))
       (is (false? (bloom-filter-test bf 100))))))
 
-(deftest test-add
+(deftest test-bloom-filter-add
   (testing "Elements added to the filter should always return true, i.e. no false negatives"
     (let [bf (-> (bloom-filter 100 5)
                  (bloom-filter-add 1)
@@ -18,3 +18,23 @@
       (is (true? (bloom-filter-test bf 1)))
       (is (true? (bloom-filter-test bf 2)))
       (is (true? (bloom-filter-test bf 100))))))
+
+(deftest test-count-min-sketch-empty
+  (testing "An empty Count-Min Sketch should return 0 for all queries"
+    (let [cms (count-min-sketch 100 5)]
+      (is (= 0 (count-min-sketch-get cms 1)))
+      (is (= 0 (count-min-sketch-get cms 2)))
+      (is (= 0 (count-min-sketch-get cms 100))))))
+
+(deftest test-basic-counting
+  (testing "Should accurately track counts threading state functionally."
+    (let [cms (count-min-sketch 1000 5)
+          cms (reduce count-min-sketch-add
+                      cms
+                      (repeat 5 11))
+          cms (reduce count-min-sketch-add 
+                      cms
+                      (repeat 3 22))]
+      (is (= 5 (count-min-sketch-get cms 11)))
+      (is (= 3 (count-min-sketch-get cms 22)))
+      (is (= 0 (count-min-sketch-get cms 33))))))
